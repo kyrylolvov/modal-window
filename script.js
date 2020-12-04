00b88758355b2582f113a66586499a3e8ef5016e
Kyrylo Lvov <70720713+kyrylolvov@users.noreply.github.com>
2020-12-04 09:06:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnClose = document.querySelector(`.close-modal`);
+const showModal = document.querySelectorAll(`.show-modal`);
+
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+
+const openModal = function () {
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener(`click`, openModal);
+}
+
+btnClose.addEventListener(`click`, closeModal);
+overlay.addEventListener(`click`, closeModal);
+
+document.addEventListener(`keydown`, function (e) {
+  if (e.key === `Escape`) {
+    modal.classList.add(`hidden`);
+    overlay.classList.add(`hidden`);
+  }
+});
 
